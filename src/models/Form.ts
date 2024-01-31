faf06759b21d28193374f2cca7fa53e29b190dcb
Leandro Del Arco <leandrodelarco@outlook.com>
2024-01-31 14:35:44 -0300

    implement form model

---
+++ src/models/Form.ts
@@ -0,0 +1,23 @@
+import { IField } from "../interfaces/IField"
+
+export class Form {
+
+    constructor(
+        public id: number = -1,
+        public name: string = "untitled",
+        public fields: Array<IField> = [],
+    ) { }
+
+    public describe(): string {
+
+        return `Form ${this.id}: ${this.name}`
+    }
+
+    // public describe(): string {
+
+    //     const description = `Form ${this.id}: ${this.name}`
+    //         + this.fields.map(field => field.describe()).join("\n")
+
+    //     return description
+    // }
+}

45ae750df4d92ed27407555ec5498b456ae58b83
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-15 13:54:25 -0300

    add order property to number field

--- src/models/fields/NumberField.ts
+++ src/models/fields/NumberField.ts
@@ -13,10 +13,11 @@ export class NumberField implements IField<number> {
         public value: number | null = null,
         public displayValue: number | null = null,
         public nullable: boolean = true,
+        public order: number | null = null,
     ) { }
 
     public describe(): string {
-        return `Field ${this.id}: ${this.name} (${FieldType[this.type]})`
+        return `Field ${this.id}: ${this.name} (${this.type})`
     }
 
     public validate(): boolean {
